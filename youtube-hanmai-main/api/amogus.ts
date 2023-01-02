28018e3ec722da4621cd2c63667f6168a0e5bf8e
terngfoxid <terngfoxid@live.jp>
2023-01-03 02:51:03 +0700

    remove han

--- youtube-hanmai-main/api/amogus.ts
+++
@@ -1,9 +0,0 @@
-import { VercelApiHandler } from '@vercel/node'
-
-const api: VercelApiHandler = async (req, res) => {
-  return res.status(200).send({
-    message: 'amogus',
-  })
-}
-
-export default api
--- youtube-hanmai-main/src/core/services/useTitle.ts
+++
@@ -1,6 +0,0 @@
-export const useTitle = (title?: string) => {
-  console.log('useTitle')
-  document.querySelector('title').innerHTML = title
-    ? `${title} · Han at karen champ`
-    : 'Han at karen champ'
-}
