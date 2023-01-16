8ce3ac82ebb9f3e6f22e64d511211a927ac3d006
terngfoxid <terngfoxid@live.jp>
2023-01-16 23:09:31 +0700

    delete unuse const

--- azur-lane-guide/components/button/B_Ship_Card.tsx
+++ azur-lane-guide/components/button/B_Ship_Card.tsx
@@ -4,7 +4,6 @@ export default function B_Ship_Card() {
 
     const card_style = (
         {
-            title: 'ข้อมูลเรือ',
             title_style: 'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-zinc-700 dark:text-zinc-300 text-3xl font-bold text-center',
             shape: "relative w-11/12 rounded-lg shadow-md border bg-neutral-200 border-gray-300 dark:border-gray-800 dark:bg-neutral-800 border-b-4 border-t-1 border-l-1 border-r-1",
             body_style: "py-2 text-zinc-600 dark:text-zinc-400 text-lg md:text-2xl text-center ",
