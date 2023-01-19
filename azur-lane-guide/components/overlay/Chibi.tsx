e65310983e003a534b490e4f8babe23fcd09fdf6
terngfoxid <terngfoxid@live.jp>
2023-01-19 23:31:50 +0700

    adjust chibi left in small screen

--- azur-lane-guide/components/overlay/Chibi.tsx
+++ azur-lane-guide/components/overlay/Chibi.tsx
@@ -3,14 +3,14 @@ export default function Chibi(chibi: any) {
     
     const chibi_style = (
         {
-            set_overlay: "fixed bottom-3 left-0 scale-50 md:scale-100 md:bottom-5 md:left-5 ",
+            set_overlay: "fixed bottom-5 left-0 md:bottom-5 md:left-5 w-full",
         }
     );
 
     return (
         <div id="shipchibi" className={chibi_style.set_overlay}>
-            <div className="flex justify-start"> 
-            <img src={"https://drive.google.com/uc?export=view&id=" + chibi.chibi} alt='ship chibi image' />
+            <div className="flex justify-items-start w-full"> 
+            <img className="w-1/6" src={"https://drive.google.com/uc?export=view&id=" + chibi.chibi} alt='ship chibi image' />
             </div>
         </div>
     )
