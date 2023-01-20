748d126ffec68afce22f1d0ae0b0226fbe7b33f7
terngfoxid <terngfoxid@live.jp>
2023-01-20 19:17:44 +0700

    add just can not click dark mode because chibi

--- azur-lane-guide/components/overlay/Chibi.tsx
+++ azur-lane-guide/components/overlay/Chibi.tsx
@@ -3,14 +3,14 @@ export default function Chibi(chibi: any) {
     
     const chibi_style = (
         {
-            set_overlay: "fixed bottom-5 left-0 md:bottom-5 md:left-5 w-full",
+            set_overlay: "fixed bottom-5 left-0 md:bottom-5 md:left-5",
         }
     );
 
     return (
         <div id="shipchibi" className={chibi_style.set_overlay}>
-            <div className="flex justify-items-start w-full"> 
-            <img className="w-1/6 md:w-max" src={"https://drive.google.com/uc?export=view&id=" + chibi.chibi} alt='ship chibi image' />
+            <div className="flex justify-items-start"> 
+            <img className="w-1/2 md:w-max" src={"https://drive.google.com/uc?export=view&id=" + chibi.chibi} alt='ship chibi image' />
             </div>
         </div>
     )
