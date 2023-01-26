3873ed2a0dd552088e68ae026dcd31b18de4c789
terngfoxid <terngfoxid@live.jp>
2023-01-26 22:00:00 +0700

    update component event page

--- azur-lane-guide/components/Active_Event_Card.tsx
+++ azur-lane-guide/components/Active_Event_Card.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import Loading from "./overlay/Loading";
 import Chibi_Event from "./overlay/Chibi_Event";
 import 'animate.css';
+import Link from "next/link";
+import { buffer } from "stream/consumers";
 
 export default function Active_Event_Card(ship: any) {
 
@@ -12,8 +14,10 @@ export default function Active_Event_Card(ship: any) {
             chibi: null,
             event_guide: null,
             name: null,
-            newship: null,
-            newship_chibi: null,
+            newship: [],
+            newship_chibi: [],
+            newship_name:[],
+            newship_type:[],
             error:null,
         }
     });
@@ -43,24 +47,73 @@ export default function Active_Event_Card(ship: any) {
     else {
         const card_style = (
             {
-                title_style: 'text-zinc-700 dark:text-zinc-300 text-2xl font-bold text-center',
+                title_style: 'py-2 text-zinc-700 dark:text-zinc-300 text-2xl font-bold text-center',
                 shape: "w-11/12 md:w-5/6 2xl:w-2/3 rounded-lg shadow-md border bg-neutral-200 border-gray-300 dark:border-gray-700 dark:bg-neutral-800",
                 position: "flex justify-center",
                 body_style: "text-zinc-600 dark:text-zinc-400 text-2xl text-center",
+                button_style: "w-11/12 rounded-lg bg-neutral-300 dark:bg-neutral-700 hover:bg-neutral-400 dark:hover:bg-neutral-600 shadow duration-300",
+
             }
         );
+
+        const ship_list = [];
+            let count = 0;
+            for (count = 0; count < eventdata.data.newship.length; count++) {
+                const buffer = count
+                ship_list.push(
+                    <div className={"flex justify-center"}>
+                        <div className={card_style.button_style}>
+                            <div>
+                                <Link className={card_style.body_style} href={"/ship/" + eventdata.data.newship[buffer]}>
+                                    <div className="flex justify-start items-center w-full">
+                                        <img src={"/images/type/" + eventdata.data.newship_type[buffer] + ".png"} alt='type image' width="50" />
+                                        <div className="truncate inline-block rounded bg-neutral-400 dark:bg-neutral-600 w-full">
+                                            <p className="max-w-fit">&nbsp;{eventdata.data.newship[buffer]}</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        {eventdata.data.newship_chibi[buffer] != null ? <>
+                                            <div className="w-full flex justify-center items-center aspect-square md:aspect-video">
+                                                <img src={"https://drive.google.com/uc?export=view&id=" + eventdata.data.newship_chibi[buffer]} alt='ship chibi image' />
+                                            </div></> : <></>
+                                        }
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
 
         return (
             <div>
                 <div id="shipdata" className={card_style.position}>
                     <div className={card_style.shape}>
-                        <h1 className={card_style.title_style}>
-                            {eventdata.data.name}
-                        </h1>
+                        <img className="object-scale-down rounded-lg" src={"https://drive.google.com/uc?export=view&id=" + eventdata.data.banner} alt={eventdata.data.banner + " picture"}></img>
                     </div>
-
                 </div>
+                <br></br>
+                
+                <div id="shipdata" className={card_style.position}>
+                    <div className={card_style.shape}>
+                        <div className="flex justify-center">
+                        <h1 className={card_style.title_style}>เรือใหม่</h1>
+                        </div>
+                        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-2">
+                            {ship_list}
+                        </div>
+                        <br></br>
+                    </div>
+                </div>
+                <br></br>
 
+                <div id="shipdata" className={card_style.position}>
+                    <div className={card_style.shape}>
+                        <div className="flex justify-center">
+                        <h1 className={card_style.title_style}>เทส123</h1>
+                        </div>
+                    </div>
+                </div>
 
                 {eventdata.data.chibi != null ? <>
                     <Chibi_Event chibi={eventdata.data.chibi} />
