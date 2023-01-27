0a3aa1f61efd93e532c027ece582480bbe5ff966
terngfoxid <terngfoxid@live.jp>
2023-01-27 23:51:38 +0700

    test home picture

--- azur-lane-guide/components/Home_Card.tsx
+++ azur-lane-guide/components/Home_Card.tsx
@@ -3,10 +3,13 @@ export default function Home_Card() {
 
     const card_style = (
         {
-            title_style: 'text-zinc-700 dark:text-zinc-300 text-2xl font-bold text-center',
-            shape: "w-11/12 md:w-5/6 2xl:w-2/3 rounded-lg shadow-md border bg-neutral-200 border-gray-300 dark:border-gray-700 dark:bg-neutral-800",
+            title_style: 'text-zinc-700 dark:text-zinc-200 text-2xl md:text-2xl lg:text-3xl font-bold text-center',
+
+            shape: "bg-cover bg-center bg-neutral-200 w-11/12 md:w-5/6 2xl:w-2/3 rounded-lg shadow-md border bg-neutral-200 border-gray-300 dark:border-gray-700 dark:bg-neutral-800",
+            //bg-neutral-200 dark:bg-neutral-800 test-bg.jpg bg-[url('/images/test-bg.jpg')] aspect-video\
+            subshape:"bg-gradient-to-b from-black/0 via-zinc-700/0 to-black/0 rounded-md ",
             position: "flex justify-center",
-            body_style: "py-2 text-zinc-600 dark:text-zinc-400 text-lg md:text-2xl text-center",
+            body_style: "py-2 text-zinc-600 dark:text-zinc-300 text-xs md:text-base lg:text-2xl text-center",
             button_style: "rounded bg-neutral-300 hover:bg-neutral-400 dark:bg-neutral-700 dark:hover:bg-neutral-600 border border-gray-400 dark:border-gray-600 text-zinc-700 dark:text-zinc-300 py-1 px-1 duration-300 hover:animate-pulse",
         }
     );
@@ -26,9 +29,10 @@ export default function Home_Card() {
     return (
         <div className={card_style.position}>
             <div className={card_style.shape}>
+                <div className={card_style.subshape}>                    
                 <br></br>
                 <h1 className={card_style.title_style}>
-                    <div className="md:flex md:justify-center md:items-center md:text-3xl">
+                    <div className="md:flex md:justify-center md:items-center">
                         <p>ยินดีต้อนรับเข้าสู่</p>
                         <p className="hidden md:block">&nbsp;</p>
                         <p>Azur Lane Guide TH</p>
@@ -39,7 +43,7 @@ export default function Home_Card() {
                 <br className="hidden md:block"></br>
                 <div className={card_style.body_style}>
                     <div className="flex justify-center">
-                        <div className="w-11/12 md:w-11/12 text-xs md:text-2xl">
+                        <div className="w-11/12 md:w-11/12">
                             <div className="md:grid md:grid-cols-3">
                                 <div className="md:col-span-2">
                                     <p className="py-1 ">เว็บไซต์นี้จัดทำเพื่อสนับสนุนผู้เล่นเกม Azur Lane ชาวไทย</p>
@@ -65,6 +69,7 @@ export default function Home_Card() {
                     </div>
                 </div>
                 <br className="hidden md:block"></br>
+            </div>
             </div>
         </div>
     )
